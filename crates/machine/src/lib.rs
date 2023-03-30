b33c7f8f47d6a14466161445d724b966f9e0b15d
Kristian Karl <kristian.hermann.karl@gmail.com>
2023-03-30 19:01:57 +0200

    WIP on the fullfilment algo. Multiple models does not work

--- crates/machine/src/lib.rs
+++ crates/machine/src/lib.rs
@@ -28,7 +28,7 @@ impl Profile {
         Profile { steps: Vec::new() }
     }
 
-    fn push(&mut self, step: Step ) {
+    fn push(&mut self, step: Step) {
         log::trace!("{:?}", step.clone());
         self.steps.push(step);
     }
@@ -55,6 +55,24 @@ pub struct Machine {
     start_context_id: Option<String>,
     start_element_id: Option<String>,
     status: MachineStatus,
+    unvisited_elements: HashMap<String, u32>,
+}
+
+fn step(step: Step, unvisited_elements: &mut HashMap<String, u32>, profile: &mut Profile) {
+    match unvisited_elements.get(&(step.context_id.clone() + &step.element_id.clone())) {
+        Some(value) => {
+            let visited = value + 1;
+            unvisited_elements.insert(step.context_id.clone() + &step.element_id.clone(), visited);
+        }
+        None => {
+            log::error!(
+                "Expected the key {}{} to be found in unvisited_elements",
+                step.context_id,
+                step.element_id
+            );
+        }
+    }
+    profile.push(step);
 }
 
 impl Machine {
@@ -66,6 +84,7 @@ impl Machine {
             start_context_id: None,
             start_element_id: None,
             status: MachineStatus::NotStarted,
+            unvisited_elements: HashMap::new(),
         }
     }
 
@@ -73,48 +92,26 @@ impl Machine {
         self.profile.clone()
     }
 
-    fn is_visited(&self, context_id: String, element_id: String) -> bool {
-        if self
-            .profile
-            .steps
-            .as_slice()
-            .into_iter()
-            .filter(|s| s.context_id == context_id && s.element_id == element_id)
-            .collect::<Vec<_>>()
-            .len()
-            > 0
-        {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
     /*
      * Calculates if the machine has covered the models given their stop conditions
      */
     fn get_fullfilment(&self) -> Option<f32> {
-        let mut edges_count: f32 = 0.;
-        let mut visited_edges_count: f32 = 0.;
+        let mut element_count = self.unvisited_elements.len();
 
-        for context in &self.contexts {
-            let context = &context.1;
-            edges_count += context.model.edges.len() as f32;
-
-            visited_edges_count += context
-                .model
-                .edges
-                .iter()
-                .filter(|(id, _edge)| {
-                    self.is_visited(context.id.clone(), (**id.clone()).to_string())
-                })
-                .collect::<Vec<_>>()
-                .len() as f32;
-        }
-        if edges_count == 0. {
+        if element_count == 0 {
+            log::error!("No elements in the models. This was unexpected");
             return None;
         }
-        return Some(visited_edges_count / edges_count);
+
+        let visited_count = self
+            .unvisited_elements
+            .iter()
+            .filter(|(_key, visits)| visits > &&0)
+            .collect::<Vec<_>>()
+            .len();
+
+        log::trace!("Visited elements: {}/{}", visited_count, element_count);
+        return Some(visited_count as f32 / element_count as f32);
     }
 
     /**
@@ -125,6 +122,7 @@ impl Machine {
     pub fn has_next(&mut self) -> Option<bool> {
         match self.get_fullfilment() {
             Some(full_filment) => {
+                log::debug!("Fullfilment is: {}", full_filment);
                 if full_filment < 1. {
                     return Some(true);
                 } else {
@@ -147,36 +145,56 @@ impl Machine {
          * There can only be one starting point in a machine.
          */
         if self.status == MachineStatus::NotStarted {
-            if self.verify_valid_start_context() {
-                self.status = MachineStatus::Running;
-                self.current_context_id = self.start_context_id.clone();
-                match self
-                    .contexts
-                    .get_mut(&self.start_context_id.clone().unwrap())
-                {
-                    Some(context) => {
-                        context.current_element_id = self.start_element_id.clone();
-                        self.profile.push(Step {
-                            context_id: context.id.clone(),
-                            element_id: context.current_element_id.clone().unwrap(),
-                        });
-                        return Some(context.current_element_id.clone().unwrap());
-                    }
-                    None => return None,
-                }
-            } else {
+            if !self.verify_valid_start_context() {
+                log::error!("No valid Context is not defined here. This was unexpected.");
                 return None;
             }
+
+            self.status = MachineStatus::Running;
+            self.current_context_id = self.start_context_id.clone();
+            let context = self
+                .contexts
+                .get_mut(&self.start_context_id.clone().unwrap());
+
+            if context.is_none() {
+                log::error!("Context is not defined here. This was unexpected.");
+                return None;
+            }
+
+            log::debug!(
+                "Models has valid start context and start elements: {:?}, {:?}",
+                self.start_context_id.as_deref(),
+                self.start_element_id.as_deref()
+            );
+
+            let mut ctx = context.unwrap();
+            ctx.current_element_id = self.start_element_id.clone();
+            step(
+                Step {
+                    context_id: ctx.id.clone(),
+                    element_id: ctx.current_element_id.clone().unwrap(),
+                },
+                &mut self.unvisited_elements,
+                &mut self.profile,
+            );
+            return Some(ctx.current_element_id.clone().unwrap());
         } else if self.status == MachineStatus::Running {
             if self.current_context_id.is_none() {
                 self.status = MachineStatus::Ended;
                 return None;
             }
+
+            let spare_list = self.contexts.clone();
             match self
                 .contexts
                 .get_mut(&self.current_context_id.clone().unwrap())
             {
                 Some(context) => {
+                    log::trace!(
+                        "Current model and element are: {:?}, {:?}",
+                        self.current_context_id.as_deref(),
+                        context.current_element_id.as_deref()
+                    );
                     /*
                      * Is the current element an edge, and does is exist in the mdoel?
                      */
@@ -192,10 +210,14 @@ impl Machine {
                             .get(&context.current_element_id.clone().unwrap())
                             .unwrap();
                         context.current_element_id = e.target_vertex_id.clone();
-                        self.profile.push(Step {
-                            context_id: context.id.clone(),
-                            element_id: context.current_element_id.clone().unwrap(),
-                        });
+                        step(
+                            Step {
+                                context_id: context.id.clone(),
+                                element_id: context.current_element_id.clone().unwrap(),
+                            },
+                            &mut self.unvisited_elements,
+                            &mut self.profile,
+                        );
                         return e.target_vertex_id.clone();
                     }
                     /*
@@ -216,14 +238,76 @@ impl Machine {
                         {
                             Some(vertex) => {
                                 if vertex.shared_state.is_some() {
-                                    // let mut list = Vec::<&Context>::new();
-                                    // for (_key, context) in self.contexts {
-                                    //     for (_key, other_vertex) in context.model.vertices.iter() {
-                                    //         if other_vertex.shared_state.is_some() && other_vertex.shared_state.clone().unwrap() == vertex.shared_state.clone().unwrap() {
-                                    //             list.push(&context);
-                                    //         }
-                                    //     }
-                                    // }
+                                    let mut list_of_same_shared_states =
+                                        Vec::<(String, String)>::new();
+
+                                    for (_key, spare_list_context) in spare_list {
+                                        log::trace!(
+                                            "Checking in model: {:?} for {:?}",
+                                            spare_list_context.model.name.as_deref(),
+                                            vertex.shared_state.as_deref()
+                                        );
+                                        for (_key, other_vertex) in
+                                            spare_list_context.model.vertices.iter()
+                                        {
+                                            if other_vertex.shared_state.as_deref()
+                                                == vertex.shared_state.as_deref()
+                                            {
+                                                list_of_same_shared_states.push((
+                                                    spare_list_context.id.clone(),
+                                                    other_vertex.id.clone().unwrap(),
+                                                ));
+                                                log::trace!(
+                                                    "Adding shared state: {:?}",
+                                                    list_of_same_shared_states.last()
+                                                );
+                                            }
+                                        }
+                                    }
+                                    log::trace!(
+                                        "Matching shared states: {}",
+                                        list_of_same_shared_states.len()
+                                    );
+
+                                    // If we have shared states matches, let's pick one vertex to swicth to
+                                    if list_of_same_shared_states.len() > 1 {
+                                        let mut rng = rand::thread_rng();
+                                        let index =
+                                            rng.gen_range(0..list_of_same_shared_states.len());
+                                        log::trace!("Random number is: {}", index);
+                                        match list_of_same_shared_states.get(index) {
+                                            Some(context_vertex) => {
+                                                log::debug!(
+                                                    "Switching to shared state: {}, {}",
+                                                    context_vertex.0.clone(),
+                                                    context_vertex.1.clone()
+                                                );
+                                                step(
+                                                    Step {
+                                                        context_id: context_vertex.0.clone(),
+                                                        element_id: context_vertex.1.clone(),
+                                                    },
+                                                    &mut self.unvisited_elements,
+                                                    &mut self.profile,
+                                                );
+                                                self.current_context_id =
+                                                    Some(context_vertex.0.clone());
+                                                self.contexts
+                                                    .get_mut(&context_vertex.0.clone())
+                                                    .unwrap()
+                                                    .current_element_id =
+                                                    Some(context_vertex.1.clone());
+                                                return Some(context_vertex.1.clone());
+                                            }
+                                            None => {
+                                                log::error!(
+                                                    "Could not switch to another shared state."
+                                                );
+                                                self.status = MachineStatus::Ended;
+                                                return None;
+                                            }
+                                        }
+                                    }
                                 }
                             }
                             None => {}
@@ -236,13 +320,23 @@ impl Machine {
                                 match list.get(index) {
                                     Some(i) => {
                                         context.current_element_id = Some(i.clone());
-                                        self.profile.push(Step {
-                                            context_id: context.id.clone(),
-                                            element_id: context.current_element_id.clone().unwrap(),
-                                        });
+                                        step(
+                                            Step {
+                                                context_id: context.id.clone(),
+                                                element_id: context
+                                                    .current_element_id
+                                                    .clone()
+                                                    .unwrap(),
+                                            },
+                                            &mut self.unvisited_elements,
+                                            &mut self.profile,
+                                        );
                                         return Some(i.clone());
                                     }
                                     None => {
+                                        log::error!(
+                                            "Random selection of an edge resultes in failure"
+                                        );
                                         self.status = MachineStatus::Ended;
                                         return None;
                                     }
@@ -291,6 +385,7 @@ impl Machine {
     }
 
     pub fn load_models(&mut self, models: Models) -> Result<Option<String>, &'static str> {
+        log::debug!("Loading {} models", models.models.len());
         for (key, model) in models.models {
             self.contexts.insert(
                 key.clone(),
@@ -301,9 +396,30 @@ impl Machine {
                     current_element_id: None,
                 },
             );
+
+            // The start_element_id can be defined in one or many models, but only one value should be used.
+            // Graphwalker studio saves the same value in all models in a json file.
             if model.start_element_id.is_some() {
+                log::debug!(
+                    "Found start element: {:?}",
+                    model.start_element_id.as_deref()
+                );
                 self.start_element_id = model.start_element_id;
-                self.start_context_id = model.id;
+            }
+
+            for (k, _e) in model.edges {
+                let uk = key.clone() + &k;
+                self.unvisited_elements.insert(uk, 0);
+            }
+            for (k, _v) in model.vertices {
+                let uk = key.clone() + &k;
+                self.unvisited_elements.insert(uk, 0);
+            }
+        }
+
+        for (key, context) in &self.contexts {
+            if context.model.has_id(self.start_element_id.clone()) {
+                self.start_context_id = Some(key.clone());
             }
         }
 
@@ -355,7 +471,7 @@ mod tests {
     fn walk() {
         let mut machine = Machine::new();
         let res = machine.load_models(json::read::read(
-            "/home/krikar//dev/graphwalker-rs/models/simple.json",
+            "/home/krikar//dev/graphwalker-rs/models/petclinic.json",
         ));
         assert!(res.is_ok());
         let res = machine.walk();
